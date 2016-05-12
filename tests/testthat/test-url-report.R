4efaf5cdcab04011a10eca3993f46c5fe48e55d7
Gaurav Sood <gsood07@gmail.com>
2016-05-12 13:55:41 -0400

    testing that works?

--- tests/testthat/test-url-report.R
+++ tests/testthat/test-url-report.R
@@ -1,12 +1,14 @@
 context("Get url report")
 
 test_that("can decrypt secrets", {
-    # Skips the test if doesn't have the key to open the secure vault
-    skip_when_missing_key("secure")
 
-    # Decrypt a file stored in secure/inst/vault
-    test <- decrypt("test", vault = "virustotal")
-    set_key(test)
+    # Skips the test if doesn't have the key to open the secure vault
+    skip_on_cran()
+
+    token_file <- file("virustotal_api_key", "r")
+    token <- suppressWarnings(readLines(token_file))
+
+    set_key(token)
     report <- url_report("http://www.google.com")
   	expect_that(report, is_a("list"))
   })
