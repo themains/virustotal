d9c276084e1565bdcd3f66515cbb4d78b9635efa
***** <gsood07@gmail.com>
2021-03-18 23:14:30 -0700

    add 2 more functions

---
+++ R/get_ip_comments.R
@@ -0,0 +1,34 @@
+#' Retrieve comments for an IP address
+#'
+#' Retrieves report on a given domain, including passive DNS, urls detected by at least one url scanner. 
+#' Gives category of the domain from bitdefender.
+#' 
+#' @param domain domain name. String. Required.  
+#' @param \dots Additional arguments passed to \code{\link{virustotal_GET}}.
+#' 
+#' @return named list
+#' @export
+#' 
+#' @references \url{https://www.virustotal.com/en/documentation/public-api/}
+#'
+#' @seealso \code{\link{set_key}} for setting the API key
+#' 
+#' @examples \dontrun{
+#' 
+#' # Before calling the function, set the API key using set_key('api_key_here')
+#'    
+#' domains("http://www.google.com")
+#' domains("http://www.goodsfwrfw.com") # Domain not found
+#' }
+
+get_ip_comments <- function(ip = NULL, limit = NULL, ...) {
+
+    if (!is.character(ip)) {
+        stop("Must specify an IP address.\n")
+    }
+
+    res   <- virustotal_GET(path = paste0("ip/", domain, "/comments",
+                                             query = list(limit = limit), ...)
+
+    res
+}
---
+++ R/get_ip_info.R
@@ -0,0 +1,40 @@
+#' Retrieve information about an IP address
+#'
+#' Retrieves report on a given domain, including passive DNS, urls detected by at least one url scanner. 
+#' Gives category of the domain from bitdefender.
+#' 
+#' @param domain domain name. String. Required.  
+#' @param \dots Additional arguments passed to \code{\link{virustotal_GET}}.
+#' 
+#' @return named list with the following possible items: 
+#' \code{`BitDefender category`, undetected_referrer_samples, whois_timestamp,
+#' detected_downloaded_samples, detected_referrer_samples, `Webutation domain info`, `Alexa category`, undetected_downloaded_samples,
+#' resolutions, detected_communicating_samples, `Opera domain info`, `TrendMicro category`, categories, domain_siblings, 
+#' `BitDefender domain info`, whois, `Alexa domain info`, response_code, verbose_msg, `Websense ThreatSeeker category`, subdomains,
+#' `WOT domain info`, detected_urls, `Alexa rank`, undetected_communicating_samples, `Dr.Web category`, pcaps}
+#'   
+#' @export
+#' 
+#' @references \url{https://www.virustotal.com/en/documentation/public-api/}
+#'
+#' @seealso \code{\link{set_key}} for setting the API key
+#' 
+#' @examples \dontrun{
+#' 
+#' # Before calling the function, set the API key using set_key('api_key_here')
+#'    
+#' domains("http://www.google.com")
+#' domains("http://www.goodsfwrfw.com") # Domain not found
+#' }
+
+get_ip_info <- function(ip = NULL, limit = NULL, ...) {
+
+    if (!is.character(ip)) {
+        stop("Must specify an IP address.\n")
+    }
+
+    res   <- virustotal_GET(path = paste0("ip/", ip),
+                                             query = list(limit = limit), ...)
+
+    res
+}
